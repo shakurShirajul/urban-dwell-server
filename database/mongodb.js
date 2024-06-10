addf172c333c08827423c5b1a52dc3bf6c4ea1a3
shakurShirajul <siirajulisllam@gmail.com>
2024-06-10 21:35:41 +0600

    vercel.json

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import 'dotenv/config';
 
-const uri = `mongodb://localhost:27017/`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mydatabase.1c7whlf.mongodb.net/?retryWrites=true&w=majority&appName=myDatabase`;
+// const uri = `mongodb://localhost:27017/`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mydatabase.1c7whlf.mongodb.net/?retryWrites=true&w=majority&appName=myDatabase`;
 
 export const database = () => {
     mongoose.connect(uri, { dbName: "urbanDwell" })
